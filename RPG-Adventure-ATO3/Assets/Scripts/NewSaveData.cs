53b2b89e3d68362f33f251da62fd6cd604b3d4ec
BronzeFlame <78716276+BronzeFlame@users.noreply.github.com>
2025-05-07 15:42:35 +0800

    Forgot to save script

--- RPG-Adventure-ATO3/Assets/Scripts/NewSaveData.cs
+++ RPG-Adventure-ATO3/Assets/Scripts/NewSaveData.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 [System.Serializable]
 public class NewSaveData
 {
-        // All Player Data to Save
+    // All Player Data to Save
+    private Vector3 respawnPoint, respawnDirection;
 
 
 
     public NewSaveData()        //This will be the inital default save value
     {                                                 
-
+        
     }
 }
