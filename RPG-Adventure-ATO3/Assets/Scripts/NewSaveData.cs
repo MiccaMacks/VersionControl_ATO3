72cb53ef661cbf36cced01f5bbfefcd606f7e007
BronzeFlame <78716276+BronzeFlame@users.noreply.github.com>
2025-05-07 15:42:04 +0800

    Inital Save File System
    -Created Save File

---
+++ RPG-Adventure-ATO3/Assets/Scripts/NewSaveData.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using NUnit.Framework;
+using UnityEngine;
+[System.Serializable]
+public class NewSaveData
+{
+        // All Player Data to Save
+
+
+
+    public NewSaveData()        //This will be the inital default save value
+    {                                                 
+
+    }
+}
