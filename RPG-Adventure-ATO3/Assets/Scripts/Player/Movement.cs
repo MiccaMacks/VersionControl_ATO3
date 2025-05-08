ff42ba88a781746cc8f0055d199600421841859e
MiccaMacks <finlay.mccormack@gmail.com>
2025-05-08 09:58:46 +0800

    Movement Created
    Basic movement created, includes primitive crouch and sprint

--- RPG-Adventure-ATO3/Assets/Scripts/Player/Movement.cs
+++ RPG-Adventure-ATO3/Assets/Scripts/Player/Movement.cs
@@ -2,15 +2,24 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] float moveSpeed = 3f, sprintSpeed = 1.8f, crouchSpeed = 0.7f;
+    private CharacterController player;
+
+
+    private void Start()
     {
-        
+        player = GetComponent<CharacterController>();
+    }
+    private void Update()
+    {
+        Vector3 movement = Vector3.zero;
+        movement += transform.forward * Input.GetAxisRaw("Vertical");
+        movement += transform.right * Input.GetAxisRaw("Horizontal");
+        float currentSpeed = moveSpeed;
+        if(Input.GetKey(KeyCode.LeftShift)) { currentSpeed *= sprintSpeed; }
+        else if(Input.GetKey(KeyCode.LeftControl)) { currentSpeed *= crouchSpeed; }
+        player.Move(movement * currentSpeed * Time.deltaTime);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
