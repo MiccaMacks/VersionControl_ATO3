2950aa43019064b43ebf030112dfaa7e3c75b9ab
MiccaMacks <finlay.mccormack@gmail.com>
2025-05-08 11:17:22 +0800

    Jump, Gravity and Smoothing added
    Currently doesn't work with stats.

--- RPG-Adventure-ATO3/Assets/Scripts/Player/Movement.cs
+++ RPG-Adventure-ATO3/Assets/Scripts/Player/Movement.cs
@@ -2,9 +2,15 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    [SerializeField] float moveSpeed = 3f, sprintSpeed = 1.8f, crouchSpeed = 0.7f;
+    [SerializeField] float moveSpeed = 3f, sprintSpeed = 1.8f, crouchSpeed = 0.7f, jumpHeight = 3f;
     private CharacterController player;
+    private Vector3 velocity;
+    [SerializeField] bool grounded;
+    private float gravity = 9.8f, currentSpeed, speedVel;
 
+    //smooth variables
+    private Vector3 smoothVelocity, currentPos = Vector3.zero;
+    [SerializeField] private float smoothTime = 0.1f;
 
     private void Start()
     {
@@ -12,13 +18,42 @@ public class Movement : MonoBehaviour
     }
     private void Update()
     {
+       
         Vector3 movement = Vector3.zero;
         movement += transform.forward * Input.GetAxisRaw("Vertical");
         movement += transform.right * Input.GetAxisRaw("Horizontal");
-        float currentSpeed = moveSpeed;
-        if(Input.GetKey(KeyCode.LeftShift)) { currentSpeed *= sprintSpeed; }
-        else if(Input.GetKey(KeyCode.LeftControl)) { currentSpeed *= crouchSpeed; }
-        player.Move(movement * currentSpeed * Time.deltaTime);
+        currentPos = Vector3.SmoothDamp(currentPos, movement.normalized, ref smoothVelocity, smoothTime);
+
+        float tempSpeed = moveSpeed;
+        if(movement != Vector3.zero)
+        {
+            if (Input.GetKey(KeyCode.LeftShift)) { tempSpeed *= sprintSpeed; }
+            else if (Input.GetKey(KeyCode.LeftControl)) { tempSpeed *= crouchSpeed; }
+        }
+
+        currentSpeed = Mathf.SmoothDamp(currentSpeed, tempSpeed, ref speedVel, smoothTime);
+
+        if (player.isGrounded)
+        {
+            grounded = true;
+            velocity = Vector3.zero;
+
+            if (Input.GetKey(KeyCode.Space))
+            {
+                Debug.Log("Player Jumped!");
+                velocity.y += jumpHeight - gravity * Time.deltaTime;
+            }
+            else
+            {
+                velocity.y -= gravity * Time.deltaTime;
+            }
+        }
+        else
+        {
+            grounded = false;
+            velocity.y -= gravity * Time.deltaTime;
+        }
+        player.Move(((currentPos * currentSpeed) + velocity)* Time.deltaTime);
     }
 
 
